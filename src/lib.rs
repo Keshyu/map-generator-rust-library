f2ffce97db5dea937fbe9144140a07ed6515f706
Covrick <asdfsdev264@gmail.com>
2019-11-19 11:42:57 +0900

    Initialize cargo library package
    Define generate_map function

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::path::Path;
+use std::hash::Hash;
+use phf::PhfHash;
+use phf_shared::FmtConst;
+
+
+pub fn generate_map<Key>(
+    name: &str,
+    output_path: &str,
+    key_type: &str,
+    value_type: &str,
+    map: &[(Key, &str)],
+)
+where
+    Key: Hash + PhfHash + Eq + FmtConst
+{
+    let path = Path::new(output_path);
+    let mut file = BufWriter::new(File::create(&path).unwrap());
+
+    write!(
+        &mut file,
+        "pub static {name}: ::phf::Map<{key}, {value}> = ",
+        name = name,
+        key = key_type,
+        value = value_type,
+    )
+        .unwrap();
+
+    let mut builder = phf_codegen::Map::new();
+
+    for (key, value) in map {
+        builder.entry(key, value);
+    }
+
+    writeln!(&mut file, "{};", builder.build()).unwrap();
+}
